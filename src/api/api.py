9074669bcbe3e31bb5856fe41ffe9f677cea0681
GuilPad <guillaume.padan@wanadoo.fr>
2024-05-15 14:35:04 +0200

    Work in progress, still the resize to be done

--- src/api/api.py
+++ src/api/api.py
@@ -13,11 +13,14 @@ from typing import Dict
 from ..models.prompt2image import prompt2imageURL
 from ..models.style_transfer_cnn import style_transfer
 
+import torch
+import torchvision.transforms as transforms
+from torchvision.models import vgg19, VGG19_Weights
 app = FastAPI()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_FILE = os.path.join(BASE_DIR, 'data', 'processed', 'paintings_dataset.csv')
-CONTENT_FILE = os.path.join(BASE_DIR, 'data', 'raw', 'images', 'content', 'astronaut.png')
+CONTENT_FILE = os.path.join(BASE_DIR, 'data', 'raw', 'images', 'content', 'background3.png')
 
 class TextPrompt(BaseModel):
     prompt: str
@@ -32,24 +35,23 @@ async def get_image(prompt: TextPrompt) -> Dict[str, str]:
         df = pd.read_csv(DATA_FILE)
         style_image_url = prompt2imageURL(prompt.prompt, df)
         content_img_path = CONTENT_FILE
+        content_title=content_img_path.replace('.png','')
         style_img_path = style_image_url
-
         # Perform style transfer and get the final image object
-        final_image = style_transfer(content_img_path, style_img_path, num_steps=300, content_weight=1e5, style_weight=1e10)
-
+        final_image = style_transfer(content_img_path, style_img_path, num_steps=200,
+                       style_weight=200000, content_weight=1)
         # Convert the image object to a Base64 string
-        buffered = BytesIO()
-        final_image.save(buffered, format="JPEG")
-        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
-
+        output_img = transforms.ToPILImage()(final_image.cpu().squeeze(0))
+        output_img.save(content_title + f"_modified.jpg")
         return {
             "prompt": prompt.prompt,
             "content_image_url": content_img_path,
             "style_image_url": style_image_url,
-            "stylized_image": img_str
+
         }
 
     except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/ping")
--- src/models/style_transfer_cnn.py
+++ src/models/style_transfer_cnn.py
@@ -1,12 +1,58 @@
 import torch
+import requests
+from io import BytesIO
 from torchvision import transforms, models
+from PIL import Image
 from torch.optim import Adam
+from torchvision.utils import save_image
 from .utils.image_utils import load_image
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from PIL import Image
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+from torchvision.models import vgg19, VGG19_Weights
 
+import copy
+
+def asset_loader(image_name, device, convert_to_rgb=False):
+    image = Image.open(image_name)
+    if convert_to_rgb:
+        image = image.convert('RGB')
+    # fake batch dimension required to fit network's input dimensions
+    image = transforms.ToTensor()(image).unsqueeze(0)
+    return image.to(device, torch.float)
+
+def load_image(img_path, device, size=512, scale=None):
+    """ Load an image from a local path or a URL and prepare it for processing. """
+    # Check if img_path is a URL
+
+    if img_path.startswith('http://') or img_path.startswith('https://'):
+        response = requests.get(img_path)
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+    else:
+        image = Image.open(img_path).convert('RGB')
+
+    if scale:
+        size = int(scale * min(image.size))
+
+    loader = transforms.Compose([
+        transforms.Resize((size, size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+        
+    image = loader(image).unsqueeze(0)
+    return image
+def resize(content_img, style_img):
+    content_img_size = content_img.size()
+    style_img_resized = transforms.Resize((content_img_size[2], content_img_size[3]))(style_img)
+    return style_img_resized
 def get_features(image, model, layers=None):
     """ Extract features from the layers of the model. """
     if layers is None:
-        layers = {'0': 'conv1_1', '5': 'conv2_1', '10': 'conv3_1', '19': 'conv4_1', '21': 'conv4_2', '28': 'conv5_1'}
+        layers = {'0.2': 'conv1_1', '0.19': 'conv2_1', '0.24': 'conv3_1', '0.11': 'conv4_1', '1': 'conv4_2', '2': 'conv5_1'}
     features = {}
     x = image
     for name, layer in model._modules.items():
@@ -15,48 +61,177 @@ def get_features(image, model, layers=None):
             features[layers[name]] = x
     return features
 
-def gram_matrix(tensor):
-    """ Calculate the Gram matrix of a tensor. """
-    _, d, h, w = tensor.size()
-    tensor = tensor.view(d, h * w)
-    gram = torch.mm(tensor, tensor.t())
-    return gram
+def gram_matrix(input):
+    a, b, c, d = input.size()  # a=batch size(=1)
+    # b=number of feature maps
+    # (c,d)=dimensions of a f. map (N=c*d)
 
-def style_transfer(content_img_path, style_img_path, num_steps=300, content_weight=1e5, style_weight=1e10):
-    """ Perform style transfer from style_img to content_img, returning the result as a PIL Image. """
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    features = input.view(a * b, c * d)  # resize F_XL into \hat F_XL
 
+    G = torch.mm(features, features.t())  # compute the gram product
+
+    # we 'normalize' the values of the gram matrix
+    # by dividing by the number of element in each feature maps.
+    return G.div(a * b * c * d)
+class ContentLoss(nn.Module):
+
+    def __init__(self, target,):
+        super(ContentLoss, self).__init__()
+        # we 'detach' the target content from the tree used
+        # to dynamically compute the gradient: this is a stated value,
+        # not a variable. Otherwise the forward method of the criterion
+        # will throw an error.
+        self.target = target.detach()
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+class StyleLoss(nn.Module):
+
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()
+
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
+
+# create a module to normalize input image so we can easily put it in a
+# ``nn.Sequential``
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        # .view the mean and std to make them [C x 1 x 1] so that they can
+        # directly work with image Tensor of shape [B x C x H x W].
+        # B is batch size. C is number of channels. H is height and W is width.
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        # normalize ``img``
+        return (img - self.mean) / self.std
+def get_style_model_and_losses(style_img, content_img):
+    # desired depth layers to compute style/content losses :
+    content_layers = ['conv_4']
+    style_layers = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+    cnn = vgg19(weights=VGG19_Weights.DEFAULT).features.eval()
+    normalization_mean = torch.tensor([0.485, 0.456, 0.406])
+    normalization_std = torch.tensor([0.229, 0.224, 0.225])
+    # normalization module
+    normalization = Normalization(normalization_mean, normalization_std)
+    # just in order to have an iterable access to or list of content/style
+    # losses
+    content_losses = []
+    style_losses = []
+    # assuming that ``cnn`` is a ``nn.Sequential``, so we make a new ``nn.Sequential``
+    # to put in modules that are supposed to be activated sequentially
+    model = nn.Sequential(normalization)
+    i = 0  # increment every time we see a conv
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = 'conv_{}'.format(i)
+        elif isinstance(layer, nn.ReLU):
+            name = 'relu_{}'.format(i)
+            # The in-place version doesn't play very nicely with the ``ContentLoss``
+            # and ``StyleLoss`` we insert below. So we replace with out-of-place
+            # ones here.
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = 'pool_{}'.format(i)
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = 'bn_{}'.format(i)
+        else:
+            raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+
+        model.add_module(name, layer)
+
+        if name in content_layers:
+            # add content loss:
+            target = model(content_img).detach()
+            content_loss = ContentLoss(target)
+            model.add_module("content_loss_{}".format(i), content_loss)
+            content_losses.append(content_loss)
+
+        if name in style_layers:
+            # add style loss:
+            target_feature = model(style_img).detach()
+            style_loss = StyleLoss(target_feature)
+            model.add_module("style_loss_{}".format(i), style_loss)
+            style_losses.append(style_loss)
+
+    # now we trim off the layers after the last content and style losses
+    for i in range(len(model) - 1, -1, -1):
+        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+            break
+
+    model = model[:(i + 1)]
+
+    return model, style_losses, content_losses
+
+def get_input_optimizer(input_img):
+    # this line to show that input is a parameter that requires a gradient
+    optimizer = optim.Adam([input_img], lr=0.01)
+    return optimizer
+def style_transfer(content_img_path, style_img_path, num_steps,
+                       style_weight, content_weight):
+    """Run the style transfer."""
+    device = torch.device("cpu")
+    torch.set_default_device(device)
     content_img = load_image(content_img_path, device)
-    style_img = load_image(style_img_path, device, scale=0.5)
+    style_img = load_image(style_img_path, device)
+    style_img= resize(content_img,style_img)
+    input_img = content_img.clone()
+    print('Building the style transfer model..')
+    
+    model, style_losses, content_losses = get_style_model_and_losses(style_img, content_img)
+    # We want to optimize the input and not the model parameters so we
+    # update all the requires_grad fields accordingly
+    input_img.requires_grad_(True)
+    # We also put the model in evaluation mode, so that specific layers
+    # such as dropout or batch normalization layers behave correctly.
+    model.eval()
+    model.requires_grad_(False)
 
-    vgg = models.vgg19(weights=models.VGG19_Weights.IMAGENET1K_V1).features
-    vgg.to(device).eval()
+    optimizer = get_input_optimizer(input_img)
 
-    content_features = get_features(content_img, vgg)
-    style_features = get_features(style_img, vgg)
-    style_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
+    print('Optimizing..')
+    run = [0]
+    while run[0] <= num_steps:
+        def closure():
+            # correct the values of updated input image
+            with torch.no_grad():
+                input_img.clamp_(0, 1)
 
-    target = content_img.clone().requires_grad_(True).to(device)
-    optimizer = Adam([target], lr=0.003)
+            optimizer.zero_grad()
+            model(input_img)
+            style_score = 0
+            content_score = 0
 
-    for i in range(1, num_steps + 1):
-        target_features = get_features(target, vgg)
-        content_loss = torch.mean((target_features['conv4_2'] - content_features['conv4_2'])**2)
+            for sl in style_losses:
+                style_score += sl.loss
+            for cl in content_losses:
+                content_score += cl.loss
 
-        style_loss = 0
-        for layer in style_grams:
-            target_gram = gram_matrix(target_features[layer])
-            layer_style_loss = torch.mean((target_gram - style_grams[layer])**2)
-            style_loss += layer_style_loss / (target_features[layer].shape[1] * target_features[layer].shape[2] * target_features[layer].shape[3])
+            style_score *= style_weight
+            content_score *= content_weight
 
-        total_loss = content_weight * content_loss + style_weight * style_loss
-        optimizer.zero_grad()
-        total_loss.backward(retain_graph=(i != num_steps))
-        optimizer.step()
+            loss = style_score + content_score
+            loss.backward()
 
-        if i % 50 == 0:
-            print(f'Step {i}, Total loss: {total_loss.item()}')
+            run[0] += 1
+            if run[0] % 50 == 0:
+                print("run {}:".format(run))
+                print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                    style_score.item(), content_score.item()))
+                print()
 
-    # Convert the processed tensor back to a PIL Image
-    final_img = transforms.ToPILImage()(target.cpu().squeeze(0))
-    return final_img
+            return style_score + content_score
+
+        optimizer.step(closure)
+
+    # a last correction...
+    with torch.no_grad():
+        input_img.clamp_(0, 1)
+    return input_img
